1d546f3d32a8f7ceea29bc4e52c8710bc5d1c49f
Zuhair Haroon Khan <zk222ac@gmail.com>
2019-02-28 12:23:25 +0100

    Final version

--- DelegateSample/Program.cs
+++ DelegateSample/Program.cs
@@ -8,14 +8,46 @@ namespace DelegateSample
 {
     class Program
     {
+        // initialize delegate type  
+        public delegate int DelSquare(int i);
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            // first you need to initialize delegate type object reference
+            // pass method (square ) as a reference
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            // Code 1: Enable when you need
+            //DelSquare objRef = Square;
+            //// invoke 
+            // var result = objRef(2);
+            //Console.WriteLine(result);
+            //var result1 = objRef.Invoke(5);
+            //Console.WriteLine(result1);
+
+            // Code 2: Enable when you need
+            // anonymous method technique
+            //DelSquare objRef = delegate(int i) { return i * i; };
+            //var result = objRef.Invoke(4);
+            //Console.WriteLine(result);
+
+
+            // Code 3: Enable when you need
+            // Function Technique
+            //Func<int , int> objRef = delegate(int i) { return i * i; };
+            //var result = objRef(5);
+            //Console.WriteLine(result);
+
+            // Code 4: Enable when you need
+            // Lambda Expression
+            Func<int, int> objRef = i => i * i;
+            var result = objRef(5);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+        public static int Square(int i)
+        {
+            return i * i;
         }
     }
 }
